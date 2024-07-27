c8ef807b452a41b58c445434912a17524ece6054
Andrew <andrew.van1@live.com>
2024-07-26 22:38:45 -0400

    initial commit

---
+++ src/main/java/com/avan3/securitydemo/SecuritydemoApplication.java
@@ -0,0 +1,14 @@
+package com.avan3.securitydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+public class SecuritydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritydemoApplication.class, args);
+	}
+
+}
