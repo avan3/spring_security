b6e8fac1d786b85d6c068c57a849fa54f47300ed
Andrew <andrew.van1@live.com>
2024-07-28 07:15:20 -0400

    add withdefaults to switch from using deprecated httpbasic method

--- src/main/java/com/avan3/securitydemo/config/SecurityConfig.java
+++ src/main/java/com/avan3/securitydemo/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.avan3.securitydemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -18,7 +19,7 @@ public class SecurityConfig {
                         .requestMatchers(new AntPathRequestMatcher("/public")).permitAll()
                         .anyRequest().authenticated()
                 )
-                .httpBasic();
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
