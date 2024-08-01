7448c98aeb8b8622ee45aea2724c845e2ad9528f
Andrew <andrew.van1@live.com>
2024-08-01 19:19:11 -0400

    added jwtfilter and new userDetailsService

--- src/main/java/com/avan3/securitydemo/config/SecurityConfig.java
+++ src/main/java/com/avan3/securitydemo/config/SecurityConfig.java
@@ -1,16 +1,35 @@
 package com.avan3.securitydemo.config;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+    private final static List<UserDetails> APPLICATION_USERS = Arrays.asList(
+            new User("admin@test.com", "password", Collections.singleton(new SimpleGrantedAuthority("ROLE_ADMIN"))),
+            new User("user@test.com", "password", Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")))
+        );
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -19,19 +38,32 @@ public class SecurityConfig {
                         .requestMatchers(new AntPathRequestMatcher("/public")).permitAll()
                         .anyRequest().authenticated()
                 )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
-//
-//   @Bean
-//   public UserDetailsService userDetailsService() {
-//       UserDetails user =
-//               User.withDefaultPasswordEncoder()
-//                       .username("user")
-//                       .password("password")
-//                       .build();
-//
-//       return new InMemoryUserDetailsManager(user);
-//   }
+
+  @Bean
+  public UserDetailsService userDetailsService() {
+    //   UserDetails user =
+    //           User.withDefaultPasswordEncoder()
+    //                   .username("user")
+    //                   .password("password")
+    //                   .build();
+
+    //   return new InMemoryUserDetailsManager(user);
+    return new UserDetailsService() {
+
+        @Override
+        public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+            return APPLICATION_USERS
+                .stream()
+                .filter(user -> user.getUsername().equals(email))
+                .findFirst()
+                .orElseThrow(() -> new UsernameNotFoundException("Not user was found!"));
+        }
+         
+    };
+  }
 }
