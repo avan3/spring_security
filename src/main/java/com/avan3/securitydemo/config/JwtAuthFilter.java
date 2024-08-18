117f86d9cb191a7d554c37ffdbc819ade1ff0cf2
Andrew <andrew.van1@live.com>
2024-08-18 09:38:10 -0400

    switched back from requiredargsconstructor to autowired

--- src/main/java/com/avan3/securitydemo/config/JwtAuthFilter.java
+++ src/main/java/com/avan3/securitydemo/config/JwtAuthFilter.java
@@ -4,8 +4,8 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.RequiredArgsConstructor;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,10 +20,11 @@ import java.io.IOException;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @Component
-@RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
 
+    @Autowired
     private JwtUtils jwtUtils;
+    @Autowired
     private UserDao userDao;
 
     @Override
