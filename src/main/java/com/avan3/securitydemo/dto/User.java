6bb791c2afafe7bf19e373f4ba6414faaaffe896
Andrew <andrew.van1@live.com>
2024-08-21 17:43:57 -0400

    add User dto

---
+++ src/main/java/com/avan3/securitydemo/dto/User.java
@@ -0,0 +1,12 @@
+package com.avan3.securitydemo.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class User {
+    private String email;
+}
