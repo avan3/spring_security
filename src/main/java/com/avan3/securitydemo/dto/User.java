acfeb6b287ce35062412b9c15706491c88ab2c7c
Andrew <andrew.van1@live.com>
2024-09-02 10:20:57 -0400

    add more properties and document annotation to user class

--- src/main/java/com/avan3/securitydemo/dto/User.java
+++ src/main/java/com/avan3/securitydemo/dto/User.java
@@ -1,11 +1,17 @@
 package com.avan3.securitydemo.dto;
 
 import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Set;
 
 @Getter
 @Setter
 @RequiredArgsConstructor
 @AllArgsConstructor
+@Document(collection="user")
 public class User {
     private String email;
+    private String password;
+    private Set<String> roles;
 }
