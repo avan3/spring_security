5fc862a001eaa144e1e3d4dd98a4f1ac181930ad
Andrew <andrew.van1@live.com>
2024-09-06 23:01:57 -0400

    updated securityconfig to use bcryptpasswordencoder

--- src/main/java/com/avan3/securitydemo/config/SecurityConfig.java
+++ src/main/java/com/avan3/securitydemo/config/SecurityConfig.java
@@ -14,7 +14,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
@@ -83,8 +83,7 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-         return NoOpPasswordEncoder.getInstance();
-//        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(10);
     }
 
     final class CsrfCookieFilter extends OncePerRequestFilter {
