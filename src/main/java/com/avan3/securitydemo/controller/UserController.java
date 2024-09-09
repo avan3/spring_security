c1e0a4ba1090484524b51d187422459c6f693e87
Andrew <andrew.van1@live.com>
2024-09-09 07:52:17 -0400

    update UserController to add new endpoint for adding user

--- src/main/java/com/avan3/securitydemo/controller/UserController.java
+++ src/main/java/com/avan3/securitydemo/controller/UserController.java
@@ -2,6 +2,7 @@ package com.avan3.securitydemo.controller;
 
 import com.avan3.securitydemo.dto.User;
 import com.avan3.securitydemo.service.UserDetailsService;
+import org.apache.coyote.BadRequestException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,9 +23,12 @@ public class UserController {
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
-//    @PostMapping()
-//    public ResponseEntity<User> addUser(@RequestBody User user) {
-//        users.add(user);
-//        return new ResponseEntity<>(user, HttpStatus.OK);
-//    }
+    @PostMapping()
+    public ResponseEntity<User> addUser(@RequestBody User user) throws BadRequestException {
+        if (null == user) {
+            throw new BadRequestException("User from request is null!");
+        }
+        User returnedUser = userDetailsService.saveUser(user);
+        return new ResponseEntity<>(returnedUser, HttpStatus.OK);
+    }
 }
